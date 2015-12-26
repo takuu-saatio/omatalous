c2137c2acb289e5b4fe22c2a36cdda3066045754
Vladimir Halme <vhalme@gmail.com>
2015-12-26 12:58:11 +0100

    Removed obsolete scripts

--- docker/omatalous-pg/rebuild.sh
+++
@@ -1,1 +0,0 @@
-docker build -t vhalme/omatalous-pg .
--- docker/omatalous-pg/restart.sh
+++
@@ -1,3 +0,0 @@
-docker stop omatalous-pg
-docker rm omatalous-pg
-docker run -d -P --volumes-from omatalous-data --name omatalous-pg vhalme/omatalous-pg
