955f86c38715585cf7de39843692ab5a6e20f7d8
Vladimir Halme <vhalme@gmail.com>
2015-12-26 15:15:19 +0100

    Added --no-cache feature to build script

--- docker/omatalous-app/build.sh
+++ docker/omatalous-app/build.sh
@@ -6,8 +6,19 @@ then
   exit
 fi
 
+if [ "$1" == "--no-cache" ]
+then
+  docker build -t vhalme/omatalous-app --no-cache .
+  exit
+fi
+
 docker build -t vhalme/omatalous-app:$1 .
 if [ ! -z "$2" ]
 then
-  docker tag vhalme/omatalous-app:$1 vhalme/omatalous-app:$2
+  if [ "$2" == "--no-cache" ]
+  then
+    docker build -t vhalme/omatalous-app:$1 --no-cache .
+    exit
+  fi
+  docker tag vhalme/omatalous-app:$1 vhalme/omatalous-app:$2 $3 .
 fi
