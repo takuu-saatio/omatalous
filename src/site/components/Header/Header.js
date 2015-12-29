2f4ffbbf9be672c5702c2e32b6a2e9677c30bafb
Vladimir Halme <vhalme@gmail.com>
2015-12-29 18:57:47 +0100

    Added left nav to small screens

--- src/site/components/Header/Header.js
+++ src/site/components/Header/Header.js
@@ -6,6 +6,8 @@ import ReactIntl from "react-intl";
 import withStyles from "../../decorators/withStyles";
 import s from "./Header.scss";
 import AppBar from "material-ui/lib/app-bar";
+import LeftNav from "material-ui/lib/left-nav";
+import MenuItem from "material-ui/lib/menus/menu-item";
 import IconButton from "material-ui/lib/icon-button";
 import NavigationClose from "material-ui/lib/svg-icons/navigation/close";
 import FlatButton from "material-ui/lib/flat-button";
@@ -14,12 +16,21 @@ import Navigation from "../Navigation";
 
 import BaseComponent from "../BaseComponent";
 
+const injectTapEventPlugin = require("react-tap-event-plugin");
+injectTapEventPlugin();
+
 @withStyles(s)
 class Header extends Component {
 
-  _handleTouchTap() {
-    console.log("tip-tap");
+  constructor(props) {
+    super(props);
+    this.state = {
+      navOpen: false
+    };
   }
+
+  _handleToggle = () => this.setState({ navOpen: !this.state.navOpen });
+  _handleClose = () => this.setState({ navOpen: false });
 
   render() {
     
@@ -27,7 +38,7 @@ class Header extends Component {
       <div className={s.root}>
         <div className={s.container}>
           <div className={s.navMin}>
-            <i className="material-icons">&#xE5D2;</i>
+            <i className="material-icons" onTouchTap={() => this._handleToggle()}>&#xE5D2;</i>
           </div>
           <div className={s.logo}>
             <a className={s.brand} href="/" onClick={Link.handleClick}>
@@ -37,6 +48,13 @@ class Header extends Component {
           </div>
           <Navigation auth={this.props.auth} selection={this.props.selection} className={s.navFull} />
         </div>
+        <LeftNav open={this.state.navOpen}
+          docked={false}
+          width={200}
+          onRequestChange={(open) => this.setState({ navOpen: open })}>
+          <MenuItem onTouchTap={this._handleClose}>Tili</MenuItem>
+          <MenuItem onTouchTap={this._handleClose}>Ulos</MenuItem>
+        </LeftNav>
       </div>
     );
   }
