ea6cc3149fb1b0f6027fbf20a4a51387fc8576c0
Vladimir Halme <vhalme@gmail.com>
2016-01-21 16:54:53 +0200

    Removed GA code temporarily

--- src/site/components/Html/Html.js
+++ src/site/components/Html/Html.js
@@ -19,6 +19,8 @@ class Html extends Component {
   };
   
   trackingCode() {
+    /*
+        <script dangerouslySetInnerHTML={this.trackingCode()} />
     return ({ __html:
       `(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=` +
       `function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;` +
@@ -26,7 +28,9 @@ class Html extends Component {
       `e.src='https://www.google-analytics.com/analytics.js';` +
       `r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));` +
       `ga('create','${config.googleAnalyticsId}','auto');ga('send','pageview');`,
-    });
+      });
+      */
+     return null;
   }
   
   initialState() {
@@ -65,7 +69,6 @@ class Html extends Component {
       <body>
         <div id="app" dangerouslySetInnerHTML={{ __html: this.props.body }} />
         <script src={this.props.entry}></script>
-        <script dangerouslySetInnerHTML={this.trackingCode()} />
       </body>
       </html>
     );
