1cc86bfebfb70b24734c912029f03e980e17ff37
Vladimir Halme <vhalme@gmail.com>
2016-03-03 13:45:24 +0200

    Added GA tracking code

--- src/site/components/Html/Html.js
+++ src/site/components/Html/Html.js
@@ -18,21 +18,17 @@ class Html extends Component {
     description: "",
   };
   
-  trackingCode() {
-    /*
-        <script dangerouslySetInnerHTML={this.trackingCode()} />
+  _trackingCode() {
     return ({ __html:
-      `(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=` +
-      `function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;` +
-      `e=o.createElement(i);r=o.getElementsByTagName(i)[0];` +
-      `e.src='https://www.google-analytics.com/analytics.js';` +
-      `r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));` +
-      `ga('create','${config.googleAnalyticsId}','auto');ga('send','pageview');`,
-      });
-      */
-     return null;
+      `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+       })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+       ga('create', 'UA-74672724-1', 'auto');
+       ga('send', 'pageview');`
+    });
   }
-  
+ 
   initialState() {
     return ({ __html:
       `window.__INITIAL_STATE__ = ${JSON.stringify(this.props.initialState)};
@@ -69,6 +65,7 @@ class Html extends Component {
         <link href="/charts.css" rel="stylesheet" type="text/css" />
         <style id="css" dangerouslySetInnerHTML={{ __html: this.props.css }} />
         <script dangerouslySetInnerHTML={this.initialState()} />
+        <script dangerouslySetInnerHTML={this.trackingCode()} />
         {intlPolyfill}
       </head>
       <body>
